2e0b2f3ad8af9cab13abcce7dc4cfc38927af404
Jeremy Ellington <jme@google.com>
2013-08-12 21:23:17 -0500

    Should be working now.

--- AccelerometerTester/src/main/java/net/eminor/accelerometertester/MainActivity.java
+++ AccelerometerTester/src/main/java/net/eminor/accelerometertester/MainActivity.java
@@ -15,8 +15,14 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener, View.OnClickListener {
 
-    int accelerometerAccuracy = 0;
+    private int accelerometerAccuracy = 0;
     private SensorManager mSensorManager;
+    private float maxX = 0.0f;
+    private float maxY = 0.0f;
+    private float maxZ = 0.0f;
+    private double vector = 0.0;
+    private double maxVector = 0.0;
+    private double minVector = 999.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +31,6 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         setContentView(R.layout.activity_main);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensorManager.registerListener(
-                this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
 
         Button resetButton = (Button)findViewById(R.id.buttonReset);
         resetButton.setOnClickListener(this);
@@ -35,8 +38,21 @@ public class MainActivity extends Activity implements SensorEventListener, View.
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(
+                this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
     public void onClick(View view) {
-        // TODO
+        maxX = 0;
+        maxY = 0;
+        maxZ = 0;
+        vector = 0;
+        maxVector = 0;
+        minVector = 999;
     }
 
 
@@ -57,6 +73,23 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         float x = sensorEvent.values[0];
         float y = sensorEvent.values[1];
         float z = sensorEvent.values[2];
+        double vector = Math.sqrt(x*x + y*y + z*z);
+
+        if (x > maxX) {
+            maxX = x;
+        }
+        if (y > maxY) {
+            maxY = y;
+        }
+        if (z > maxZ) {
+            maxZ = z;
+        }
+        if (vector > maxVector) {
+            maxVector = vector;
+        }
+        if (vector < minVector) {
+            minVector = vector;
+        }
 
         TextView textViewX = (TextView)findViewById(R.id.textViewX);
         textViewX.setText(String.format("%f", x));
@@ -64,6 +97,20 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         textViewY.setText(String.format("%f", y));
         TextView textViewZ = (TextView)findViewById(R.id.textViewZ);
         textViewZ.setText(String.format("%f", z));
+        TextView textViewVector = (TextView)findViewById(R.id.textViewVector);
+        textViewVector.setText(String.format("%f", (float)vector));
+
+        TextView textViewMaxX = (TextView)findViewById(R.id.textViewMaxX);
+        textViewMaxX.setText(String.format("%f", maxX));
+        TextView textViewMaxY = (TextView)findViewById(R.id.textViewMaxY);
+        textViewMaxY.setText(String.format("%f", maxY));
+        TextView textViewMaxZ = (TextView)findViewById(R.id.textViewMaxZ);
+        textViewMaxZ.setText(String.format("%f", maxZ));
+
+        TextView textViewMaxVector = (TextView)findViewById(R.id.textViewVectorMax);
+        textViewMaxVector.setText(String.format("%f", (float)maxVector));
+        TextView textViewMinVector = (TextView)findViewById(R.id.textViewVectorMin);
+        textViewMinVector.setText(String.format("%f", (float)minVector));
 
         //mAccelLast = mAccelCurrent;
         //mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
